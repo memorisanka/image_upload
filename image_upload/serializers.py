c3bea162c9ea198d7d951bc0bda51bc9b723fd8e
memorisanka <j.krawczyk87@gmail.com>
2023-09-15 18:31:56 +0200

    feat: add serializers

---
+++ image_upload/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Tier, Image
+
+
+class TierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tier
+        fields = 'name', 'thumbnail_sizes', 'allow_original_link', 'allow_expiring_link'
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = 'user', 'image', 'tier', 'created_at', 'updated_at'
