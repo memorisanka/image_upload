789380d6a2f1b20d1fd8adb34c7de617b3797b08
memorisanka <j.krawczyk87@gmail.com>
2023-09-15 18:56:48 +0200

    refactor: add __str__ to models, make migrations

--- image_upload/models.py
+++ image_upload/models.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import JSONField
 
 
 class Tier(models.Model):
     name = models.CharField(max_length=255)
-    thumbnail_sizes = models.CharField(max_length=255)
+    thumbnail_sizes = JSONField()
     allow_original_link = models.BooleanField(default=False)
     allow_expiring_link = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Image(models.Model):
@@ -15,6 +19,9 @@ class Image(models.Model):
     tier = models.ForeignKey(Tier, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.image.name
 
 
 class UserProfile(models.Model):
