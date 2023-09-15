0e3b041fa674be0ae354e1f473b7292af683cb36
memorisanka <j.krawczyk87@gmail.com>
2023-09-15 18:27:04 +0200

    feat: create Tier model

--- image_upload/models.py
+++ image_upload/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Tier(models.Model):
+    name = models.CharField(max_length=255)
+    thumbnail_sizes = models.CharField(max_length=255)
+    allow_original_link = models.BooleanField(default=False)
+    allow_expiring_link = models.BooleanField(default=False)
