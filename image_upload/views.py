1f3787b2c8914be12607fbc5f6f1a26d8aa18c26
memorisanka <j.krawczyk87@gmail.com>
2023-09-15 19:04:23 +0200

    feat: add ImageView to views

--- image_upload/views.py
+++ image_upload/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
 
-# Create your views here.
+from .models import Image
+from .serializers import ImageSerializer
+
+
+class ImageView(generics.ListAPIView):
+    serializer_class = ImageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Image.objects.filter(user=user)
