503869c284bbb247041e7cc29a78f6edbdec2651
memorisanka <j.krawczyk87@gmail.com>
2023-09-15 19:10:28 +0200

    refactor: add brackets to serializers

--- image_upload/serializers.py
+++ image_upload/serializers.py
@@ -5,10 +5,10 @@ from .models import Tier, Image
 class TierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tier
-        fields = 'name', 'thumbnail_sizes', 'allow_original_link', 'allow_expiring_link'
+        fields = ('name', 'thumbnail_sizes', 'allow_original_link', 'allow_expiring_link')
 
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = 'user', 'image', 'tier', 'created_at', 'updated_at'
+        fields = ('user', 'image', 'tier', 'created_at', 'updated_at')
