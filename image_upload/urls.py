825ea4351f6fe4f525ee627d1609490da017c26d
memorisanka <j.krawczyk87@gmail.com>
2023-09-17 10:51:45 +0200

    feat: save image

--- image_upload/urls.py
+++ image_upload/urls.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+
 from .views import ImageUploadView, ImageView
 
 urlpatterns = [
     path('upload/', ImageUploadView.as_view(), name='image-upload'),
     path('images/', ImageView.as_view(), name='image-list'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- image_upload/views.py
+++ image_upload/views.py
@@ -1,3 +1,5 @@
+from PIL import Image as PILImage
+from django.http import HttpResponseBadRequest
 from rest_framework import generics
 from rest_framework import permissions
 
@@ -12,3 +14,45 @@ class ImageView(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return Image.objects.filter(user=user)
+
+
+class ImageUploadView(generics.CreateAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        tier = user.userprofile.tier
+
+        if not tier:
+            return HttpResponseBadRequest("User tier not specified.")
+
+        image_file = self.request.data.get('image')
+
+        if not image_file:
+            return HttpResponseBadRequest("Image not provided.")
+
+        image_instance = serializer.save(user=user)
+
+    def generate_thumbnail(self, image_instance, size):
+        try:
+            # Otwórz oryginalny obraz
+            image = PILImage.open(image_instance.image)
+
+            # Ustaw szerokość/proporcje na podstawie żądanej wielkości
+            width = size
+            height = int((float(image.size[1]) / float(image.size[0])) * float(width))
+            new_size = (width, height)
+
+            # Wygeneruj miniaturę
+            image.thumbnail(new_size, PILImage.ANTIALIAS)
+
+            # Zapisz miniaturę w polu Miniatura modelu Image
+            image_instance.thumbnail = image
+
+            # Zapisz model Image z miniaturą
+            image_instance.save()
+
+        except Exception as e:
+            print(f"Error generating thumbnail: {str(e)}")
